4a056935da66e448470e2369b4a96093a4c79e35
Dom Hallan <dom@domhallan.com>
2024-05-02 15:38:03 -0400

    Added transfer functionality to accounts
    A new function, CreateTransfer, has been added. This function inserts a new record into the transfers table with from_account_id, to_account_id and amount as parameters. A corresponding struct, CreateTransferParams, has also been defined to handle these parameters. The function returns a Transfer object upon successful execution or an error otherwise.

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -66,6 +66,35 @@ func (q *Queries) CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry
 	return i, err
 }
 
+const createTransfer = `-- name: CreateTransfer :one
+INSERT INTO transfers (
+  from_account_id,
+  to_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+) RETURNING id, from_account_id, to_account_id, amount, created_at
+`
+
+type CreateTransferParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+func (q *Queries) CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error) {
+	row := q.db.QueryRow(ctx, createTransfer, arg.FromAccountID, arg.ToAccountID, arg.Amount)
+	var i Transfer
+	err := row.Scan(
+		&i.ID,
+		&i.FromAccountID,
+		&i.ToAccountID,
+		&i.Amount,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
 const deleteAccount = `-- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1
