afba0178bda4683b4fc659807d31be53e80de891
Dom Hallan <dom@domhallan.com>
2024-05-02 15:27:44 -0400

    Updated SQL query for account updates
    The SQL update statement in the account handling code has been modified. Now, it returns additional fields (id, owner, balance, currency, created_at) after updating an account's balance. This change will provide more information about the updated account directly from the database operation.

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -98,6 +98,7 @@ const updateAccount = `-- name: UpdateAccount :exec
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
+RETURNING id, owner, balance, currency, created_at
 `
 
 type UpdateAccountParams struct {
