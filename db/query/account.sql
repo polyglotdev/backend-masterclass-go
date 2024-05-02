edd36750f7fc5282651cf717e0a53c0050e8f923
Dom Hallan <dom@domhallan.com>
2024-05-02 15:37:53 -0400

    Added new SQL query for account transfers
    A new SQL query has been added to handle account transfers. This includes inserting the 'from_account_id', 'to_account_id', and 'amount' into the 'transfers' table. The function returns all columns of the newly created transfer record.

--- db/query/account.sql
+++ db/query/account.sql
@@ -45,3 +45,12 @@ WHERE account_id = $1
 ORDER BY id
 LIMIT $2
 OFFSET $3;
+
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+  from_account_id,
+  to_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
